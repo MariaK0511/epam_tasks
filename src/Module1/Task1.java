7f363d9098135f57b0d3fbde46de9fc177bf677d
mary <mariamikhailovna2001@gmail.com>
2021-07-28 21:56:40 +0300

    create task1 and task7

--- src/Module1/Task1.java
+++ src/Module1/Task1.java
@@ -4,25 +4,20 @@ public class Task1 {
     //1. Даны два действительных числа х и у. Вычислить их сумму, разность, произведение и частное.
 
     public static void main (String [] args){
-    System.out.println("1. Даны два действительных числа х и у. Вычислить их сумму, разность, произведение и частное.");
 
-    double a = 2;
-    double b = 4;
-    double sum = a + b;
+    int a = 2;
+    int b = 4;
+        int sum = a + b;
         System.out.println("сумма чисел" + a + "+" + b + "=" + sum);
 
-    double difference = b - a;
+        double difference = b - a;
         System.out.println("разность чисел" + b + "-" + a + "=" + difference);
 
-    double composition = b * a;
+        double composition = b * a;
         System.out.println("произведение чисел"  + a + "*" + b + "=" + composition);
 
-    double quotient = b / a;
+        double quotient = b / a;
         System.out.println("частное чисел" + b + "/" + a + "=" + quotient);
-        System.out.println();
-
-
-
-}
+    }
 }
 
