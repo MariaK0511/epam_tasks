62f79715adce3f5b746481e3907f750b376ddc9f
mary <mariamikhailovna2001@gmail.com>
2021-07-29 13:28:22 +0300

    create task2 and task4

--- src/Module1/Task12.java
+++ src/Module1/Task12.java
@@ -9,12 +9,13 @@ public class Task12 {
         double y2 = 1.0;
         double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
         System.out.println("Расстояние между точками (" + x1 + ", " + y1 + ") и (" + x2 + ", " + y2 + ") равно " + length);
-
+        task13();
+        task14();
+        task15();
     }
 
-    private static void task13(String[] args) {
+    private static void task13() {
         //13. Заданы координаты трех вершин треугольника (х1, у1),(х2, у2),(х3, у3). Найти его периметр и площадь.");
-
         double x1 = 2.0;
         double x2 = 3.0;
         double x3 = 5.0;
@@ -31,22 +32,20 @@ public class Task12 {
         System.out.println("Площадь = " + s);
     }
 
-        private static void task14(String[] args) {
+    private static void task14() {
         //14. Вычислить длину окружности и площадь круга одного и того же заданного радиуса R
-            double r = 4.5;
-            double length = 2 * Math.PI * r;
-            double s = Math.PI * r * r;
-            System.out.println("Длина окружности = " + length);
-            System.out.println("Площадь круга = " + s);
-        }
-    private static void task15(){
+        double r = 4.5;
+        double length = 2 * Math.PI * r;
+        double s = Math.PI * r * r;
+        System.out.println("Длина окружности = " + length);
+        System.out.println("Площадь круга = " + s);
+    }
+
+    private static void task15() {
         //не поняла решение
         //15. Написать программу, которая выводит на экран первые четыре степени числа π.");
-
-        for (int i = 1; i <= 5; i++){
+        for (int i = 1; i <= 5; i++) {
             System.out.println("Pi^" + i + " = " + Math.pow(Math.PI, i));
         }
-
-        System.out.println();
     }
 }
